d6b7d3fe31c33fda697dc5f85da683907c1288f1
Brandt <bseibs>
2024-03-30 19:22:30 -0700

    exit function

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
 const connect = require("./client.js");
 console.log("Connecting ...");
 connect();
+const setupInput = function () {
+  const stdin = process.stdin;
+  const handleUserInput = function (key) {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  return stdin;
+};
+
+setupInput();
