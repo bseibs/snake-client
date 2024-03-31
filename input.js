9eadfe24bf3d77319b83f016eb79092742ed2cca
Brandt <bseibs>
2024-03-30 19:38:43 -0700

    successful export of setupinput and connect functions

---
+++ input.js
@@ -0,0 +1,19 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  const handleUserInput = function (key) {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  return stdin;
+};
+
+setupInput();
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,20 +1,6 @@
-const connect = require("./client.js");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
+
 console.log("Connecting ...");
 connect();
-const setupInput = function () {
-  const stdin = process.stdin;
-  const handleUserInput = function (key) {
-    if (key === "\u0003") {
-      process.exit();
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  return stdin;
-};
-
 setupInput();
