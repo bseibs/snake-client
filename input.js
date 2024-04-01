9331802fd1f07d7f49d592145beec59361a609ec
Brandt <bseibs>
2024-03-31 19:06:10 -0700

    controls working

--- input.js
+++ input.js
@@ -1,19 +1,28 @@
-const setupInput = function () {
-  const stdin = process.stdin;
-  const handleUserInput = function (key) {
-    if (key === "\u0003") {
-      process.exit();
-    }
-  };
+let connection;
 
-  stdin.on("data", handleUserInput);
+const setupInput = function (conn) {
+  connection = conn; // Assigning conn to module-wide variable connection.
+  const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
+  const handleUserInput = function (key) {
+    if (key === "\u0003") {
+      process.exit();
+    } else if (key === "w") {
+      connection.write("Move: up"); // Using the outer scope variable connection.
+    } else if (key === "a") {
+      connection.write("Move: left");
+    } else if (key === "s") {
+      connection.write("Move: down");
+    } else if (key === "d") {
+      connection.write("Move: right");
+    }
+  };
+  stdin.on("data", handleUserInput);
+
   return stdin;
 };
-
-setupInput();
 
 module.exports = { setupInput };
