24d879233ad59b7db89b469174dab03904cea4fb
Brandt <bseibs>
2024-03-31 19:49:35 -0700

    both codes available in case project needs multiple snakes

--- client.js
+++ client.js
@@ -24,3 +24,34 @@ const connect = function () {
 };
 
 module.exports = { connect };
+
+// * commenting out this code as it brings in multiple sakes. unsure if this is what the project wants.
+
+// const net = require("net");
+// const { IP, PORT } = require("./constants");
+
+// // establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: IP,
+//     port: PORT,
+//   });
+//   conn.on("connect", function () {
+//     console.log("successfully connected to game server!");
+//     conn.write("Name: BDS\n");
+//   });
+
+//   conn.on("data", function (data) {
+//     console.log("server says: ", data);
+//   });
+
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
+
+//   return conn;
+// };
+
+// console.log("Connecting ...");
+// connect();
+
+// module.exports = { connect };
